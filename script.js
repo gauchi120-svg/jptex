98869861d181b2811e3328c81b7727b4c0b60b62
gauchi120-svg <gauchi120@gmail.com>
2025-09-07 18:04:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
         loop: true, 
         autoplay: {
-            delay: 3000,
+            delay: 2000,
             disableOnInteraction: false, 
         },
         pagination: {
